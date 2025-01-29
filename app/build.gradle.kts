aadf6a7417ae0f2b6e3b5e2f3ac782d36b21bcc0
bersons <bersonintech@gmail.com>
2025-01-29 17:38:05 -0300

    - version 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "br.com.brainize"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 110
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
